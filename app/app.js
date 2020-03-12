2cec366e6e6ca7912b81a408b36dfc94126b7cf9
JohnMichelin <john.michelin@galvanize.com>
2020-03-12 09:02:50 -0700

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,41 @@
+/*
+
+ ### Basic Reqs
+- [ ] Where to store data? (localstorage)
+- [ ] How to caputure data? (web form)
+- [ ] How to modify data? (update action, delete action)
+- [ ] How to view data? (style?)
+- [ ] UI/UX considerations (how are we going to use this)
+
+*/
+
+// Local Storage Utility Functions
+//get item
+  // code goes here
+
+//create
+  // code goes here
+
+//update
+  // code goes here
+
+//delete
+  // code goes here
+
+//clear everything
+  // code goes here
+
+// key exists? 
+  // code goes here
+
+
+///////////////////////////////////////////
+$(document).ready(function() {
+
+ // click event listener for create
+    // code goes here
+
+ // click event listener for update
+   // code goes here
+ 
+});
